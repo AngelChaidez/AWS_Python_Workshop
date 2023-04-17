5e858a5887412010ec7a33ed896db17da201cde5
Angel Chaidez <angelomarchaidez@gmail.com>
2023-04-17 12:32:17 -0700

    AWS workshop complete
    AWS workshop complete

---
+++ lab_4_step_1_positional_arguments.py
@@ -0,0 +1,16 @@
+import boto3
+
+def translate_text():
+    client = boto3.client('translate')
+    response = client.translate_text(
+        Text='I am learning to code in AWS', 
+        SourceLanguageCode='en', 
+        TargetLanguageCode='es' 
+    )
+    print(response) 
+
+def main():
+    translate_text()
+
+if __name__=="__main__":
+    main()
